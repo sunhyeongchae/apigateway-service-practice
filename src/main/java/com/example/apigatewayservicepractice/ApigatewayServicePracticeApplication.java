beb2f8a0fce0687cde79227422e69e6da1a5f21b
codky <msky0912@gmail.com>
2023-04-21 18:03:16 +0900

    Initial commit

---
+++ src/main/java/com/example/apigatewayservicepractice/ApigatewayServicePracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.apigatewayservicepractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApigatewayServicePracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApigatewayServicePracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/apigatewayservicepractice/ApigatewayServicePracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apigatewayservicepractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApigatewayServicePracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
